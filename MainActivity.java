a38073c336a39481a7d087f7cead632ea2318963
sharjakhader <101325642+sharjakhader@users.noreply.github.com>
2022-03-14 14:14:12 +0530

    listview

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,23 @@
-package com.sharja.menuiteams;
+package com.sharja.listview;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
-
+	
+	ListView list;
+	String[] listArray= {"Java","Kotlin","XML","Flutter","React","Angular","Vue","Xamarin"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        list=(ListView)findViewById(R.id.listView1);
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this,
+        		android.R.layout.simple_spinner_dropdown_item,listArray);
+        list.setAdapter(adapter);
     }
 
 
