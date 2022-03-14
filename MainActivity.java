9c7b5ba9ac0782faaf207023f24b9d6a9ec48130
sharjakhader <101325642+sharjakhader@users.noreply.github.com>
2022-03-14 14:12:05 +0530

    multiplication_table

--- MainActivity.java
+++ MainActivity.java
@@ -1,47 +1,62 @@
-package com.sharja.radiobutton;
+package com.sharja.multiplication_table;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-
-
-
 public class MainActivity extends Activity {
-	
-	   RadioButton rm,rf;
-	   RadioGroup rg;
-	   TextView t;
-
-	   @Override
-     protected void onCreate(Bundle savedInstanceState) {    	
+	EditText editText;
+	Button button;
+	TextView textView1,textView2;
+	int num,mul,res;
+	String str="";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        rm=(RadioButton)findViewById(R.id.rdMale);
-        rf=(RadioButton)findViewById(R.id.rdFemale);
-        t=(TextView)findViewById(R.id.txtGender);
-        rg=(RadioGroup)findViewById(R.id.rdGender);
-        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-			
-        	
-        	@Override
-        	public void onCheckedChanged(RadioGroup group,int checkedid){
-        		if(rm.isChecked())
-        		{
-        			t.setText("you are a man");
-        		}
-        		if(rf.isChecked())
-        		{
-        			t.setText("you are now a women");
-        		}
-        		
-        		}
-        	});
-        }
+        editText=(EditText)findViewById(R.id.editText1);
+        textView1=(TextView)findViewById(R.id.textView1);
+        textView2=(TextView)findViewById(R.id.textView2);
+        button=(Button)findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+		
+			@Override
+			public void onClick(View arg0) {
+				
+				if(editText.length()>0){
+					
+					num=Integer.parseInt(editText.getText().toString());
+					textView1.setText("Multiplication Table of "+num);
+					for(int i=0;i<=10;i++){
+						mul=i;
+						res=num*mul;
+						str+= "\n"+mul+"x"+num+"="+res;
+					}
+					textView2.setText(str);
+					str="";
+				}else{
+					str="Enter Some Value";
+					textView1.setText(str);
+					str="";
+				}
+				
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
 }
-
-
-   
