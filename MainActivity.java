406ac2fb0dbabc5ab24e0d90ce6cb60b108d2099
sharjakhader <101325642+sharjakhader@users.noreply.github.com>
2022-03-18 12:25:16 +0530

    Edit_Text_Event

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,39 @@
-package com.samad.menuapp;
+package com.samad.edittextevent;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	EditText editText;
+	TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView=(TextView)findViewById(R.id.textView1);
+        editText=(EditText)findViewById(R.id.editText1);
+        editText.addTextChangedListener(new TextWatcher() {
+			
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3) {
+				// TODO Auto-generated method stub
+				String text=editText.getText().toString();
+				textView.setText(text);
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {}
+			
+			@Override
+			public void afterTextChanged(Editable arg0){}
+		});
     }
 
 
